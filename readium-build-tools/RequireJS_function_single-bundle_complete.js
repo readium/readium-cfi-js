6efe6bf7e255fe817c31e390762b8031cb78145d
danielweck <daniel.weck@gmail.com>
2015-05-15 17:54:56 +0100

    tweaked bundle generator to include root package namespaces

--- readium-build-tools/RequireJS_function_single-bundle_complete.js
+++ readium-build-tools/RequireJS_function_single-bundle_complete.js
@@ -1,14 +1,14 @@
 //  Copyright (c) 2014 Readium Foundation and/or its licensees. All rights reserved.
-//  
-//  Redistribution and use in source and binary forms, with or without modification, 
+//
+//  Redistribution and use in source and binary forms, with or without modification,
 //  are permitted provided that the following conditions are met:
-//  1. Redistributions of source code must retain the above copyright notice, this 
+//  1. Redistributions of source code must retain the above copyright notice, this
 //  list of conditions and the following disclaimer.
-//  2. Redistributions in binary form must reproduce the above copyright notice, 
-//  this list of conditions and the following disclaimer in the documentation and/or 
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//  this list of conditions and the following disclaimer in the documentation and/or
 //  other materials provided with the distribution.
-//  3. Neither the name of the organization nor the names of its contributors may be 
-//  used to endorse or promote products derived from this software without specific 
+//  3. Neither the name of the organization nor the names of its contributors may be
+//  used to endorse or promote products derived from this software without specific
 //  prior written permission.
 
 function(data) {
@@ -29,25 +29,63 @@ function(data) {
 
         var path = config.modules[i].out; //config.modules[i].layer.buildPathMap[config.modules[i].name];
         console.log(path);
-        
+
         // var shortpath = path.replace(rootPath, './');
         // console.log(shortpath);
-        
+
         // var pathConfig = {};
         // pathConfig[config.modules[i].name] = shortpath;
-        
+
         var bundleConfig = {};
         bundleConfig[config.modules[i].name] = [];
-        
+
         for (var moduleName in config.modules[i].layer.modulesWithNames) {
             bundleConfig[config.modules[i].name].push(moduleName);
             console.log(">> " + moduleName);
         }
         for (var moduleName in config.modules[i].layer.needsDefine) {
-            bundleConfig[config.modules[i].name].push(moduleName);
-            console.log(">> " + moduleName);
+
+            var found = false;
+            for (var k = 0; k < bundleConfig[config.modules[i].name].length; k++)
+            {
+                var alreadyIncluded = bundleConfig[config.modules[i].name][k];
+                if (alreadyIncluded == moduleName)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                bundleConfig[config.modules[i].name].push(moduleName);
+                console.log(">> " + moduleName);
+            }
         }
-        
+
+        if (config.modules[i].include)
+        {
+            for (var j = 0; j < config.modules[i].include.length; j++)
+            {
+                var included = config.modules[i].include[j];
+
+                var found = false;
+                for (var k = 0; k < bundleConfig[config.modules[i].name].length; k++)
+                {
+                    var alreadyIncluded = bundleConfig[config.modules[i].name][k];
+                    if (alreadyIncluded == included)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    bundleConfig[config.modules[i].name].push(included);
+                    console.log(">> " + included);
+                }
+            }
+        }
+
         fs.writeFile(
             path + ".bundles.js",
             "require.config({" +
