fd4dc7b41742cae8aea50d8117cbea6c7219ed32
danielweck <daniel.weck@gmail.com>
2015-07-14 13:08:41 +0100

    RequireJS updated to 2.1.19 => no need for patch

--- readium-build-tools/patchRequireJS.js
+++ readium-build-tools/patchRequireJS.js
@@ -6,15 +6,16 @@ var rjsPath = path.join(process.cwd(), 'node_modules/requirejs/bin/r.js');
 console.log("Patching RequireJS optimizer (duplicate sourceMappingURL bug): ");
 console.log(rjsPath);
 
-    // https://github.com/jrburke/r.js/issues/807
-    // https://github.com/jrburke/r.js/issues/802
-
 var rjs = fs.readFileSync(rjsPath, {encoding: 'utf8'});
 fs.writeFileSync(rjsPath,
+
+    // https://github.com/jrburke/r.js/issues/807
+    // https://github.com/jrburke/r.js/issues/802  
+  // fixed in 2.1.19
+  // http://requirejs.org/docs/download.html#releasenotes
+  //rjs.replace(' stream += "\\n//# sourceMappingURL=" + options.outSourceMap;', ' //stream += "\\n//# sourceMappingURL=" + options.outSourceMap;')
   
-  rjs.replace(' stream += "\\n//# sourceMappingURL=" + options.outSourceMap;', ' //stream += "\\n//# sourceMappingURL=" + options.outSourceMap;')
-  
-  .replace('sourceMapGenerator.setSourceContent(sourceMapPath, singleContents);', 'if ((typeof config.sourceMapIncludeSources === "undefined") || config.sourceMapIncludeSources) sourceMapGenerator.setSourceContent( sourceMapPath,singleContents );')
+  rjs.replace('sourceMapGenerator.setSourceContent(sourceMapPath, singleContents);', 'if ((typeof config.sourceMapIncludeSources === "undefined") || config.sourceMapIncludeSources) sourceMapGenerator.setSourceContent( sourceMapPath,singleContents );')
   
   .replace('resultMap = finalMap.toString();', 'resultMap=finalMap.toString() ; if ((typeof uconfig.sourceMapIncludeSources !== "undefined") && !uconfig.sourceMapIncludeSources) { resultMap = resultMap.replace(/,"sourcesContent":\\[[\\s\\S]*/, "}"); }')
   /*
