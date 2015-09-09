a444bcb1fd37a431398e0dedd14a693896491eec
danielweck <daniel.weck@gmail.com>
2015-09-09 22:22:55 +0100

    Harmonization of Node fs.existsSync and fs.accessSync usage. Added logfile feature to HTTP server script (to avoid spamming the shell console / command line), and added web-browser-opener optional parameter (to control which web page to display once the HTTP server is running)

--- readium-build-tools/http-server.js
+++ readium-build-tools/http-server.js
@@ -999,10 +999,9 @@ var localIP = function ()
   return undefined;
 }
 
-var IP = localIP();
-console.log(IP);
-
 var args = process.argv.slice(2);
+
+
 
 var PORT = 7777;
 
@@ -1014,35 +1013,129 @@ for (var i = 0; i < args.length; i++) {
     }
 }
 
-args.unshift(IP);
-args.unshift("-a");
+
+var PATH = undefined;
+
+for (var i = 0; i < args.length; i++) {
+    if (args[i] === "-OPEN") {
+        args.splice(i, 1);
+        
+        if (i < args.length) {
+            PATH = args[i];
+            args.splice(i, 1);
+        }
+        
+        console.log(PATH);
+        break;
+    }
+}
+
+
+var LOGFILE = undefined;
+
+for (var i = 0; i < args.length; i++) {
+    if (args[i] === "-LOGFILE") {
+        args.splice(i, 1);
+        
+        if (i < args.length) {
+            LOGFILE = args[i];
+            args.splice(i, 1);
+        }
+        
+        console.log(LOGFILE);
+        break;
+    }
+}
+
+
+var IP = undefined;
+
+for (var i = 0; i < args.length; i++) {
+    if (args[i] === "-a") {
+        if (++i < args.length) IP = args[i];
+        break;
+    }
+}
+
+if (!IP) {
+    IP = localIP();
+    
+    args.unshift(IP);
+    args.unshift("-a");
+}
+console.log(IP);
+
+
 
 console.log("http-server.js arguments: ");
 console.log(args);
 
+var fs = require('fs');
 var path = require('path');
 var child_process = require('child_process');
 
 args.unshift(path.join(process.cwd(), 'node_modules', 'http-server', 'bin', 'http-server'));
 
+
+
+// fs.existsSync is marked as deprecated, so accessSync is used instead (if it's available in the running version of Node).
+function doesFileExist(path) {
+    var exists;
+    if (fs.accessSync) {
+        try {
+            fs.accessSync(path);
+            exists = true;
+        } catch (ex) {
+            exists = false;
+        }
+    } else {
+        exists = fs.existsSync(path);
+    }
+    return exists;
+}
+
+if (LOGFILE) {
+    LOGFILE = path.join(process.cwd(), LOGFILE); 
+        
+    if (doesFileExist(LOGFILE)) {
+        console.log("~~ delete: " + LOGFILE);
+        fs.unlinkSync(LOGFILE);
+    }
+}
+
 var child = child_process.spawn('node', args);
 child.stdout.on('data', function(data) {
-    console.log(data.toString());
+    if (LOGFILE) {
+        fs.appendFileSync(LOGFILE, data.toString());
+    } else {
+        console.log(data.toString());
+    }
 });
 child.stderr.on('data', function(data) {
-    console.log(data.toString());
+    if (LOGFILE) {
+        fs.appendFileSync(LOGFILE, data.toString());
+    } else {
+        console.log(data.toString());
+    }
 });
 child.on('close', function(code) {
-  console.log('HTTP child process exit code: ' + code);
+    console.log('HTTP child process exit code: ' + code);
+    if (LOGFILE) {
+        console.log("HTTP log: " + LOGFILE);
+    }
 });
 
-var child2 = child_process.spawn('node', ['node_modules/opener/opener.js', 'http://'+IP+':'+PORT+'/']);
-child2.stdout.on('data', function(data) {
-    console.log(data.toString());
-});
-child2.stderr.on('data', function(data) {
-    console.log(data.toString());
-});
-child2.on('close', function(code) {
-  console.log('OPENER child process exit code: ' + code);
-});
+
+
+if (PATH) {
+    var child2 = child_process.spawn('node', ['node_modules/opener/opener.js', 'http://'+IP+':'+PORT+PATH]);
+    child2.stdout.on('data', function(data) {
+        console.log(data.toString());
+    });
+    child2.stderr.on('data', function(data) {
+        console.log(data.toString());
+    });
+    child2.on('close', function(code) {
+        console.log('OPENER child process exit code: ' + code);
+    });
+}
