fe7ec0f4a721827ff8eea47c6350804dc2702b7e
Juan Corona <juanc@evidentpoint.com>
2018-06-21 11:27:23 -0700

    Simplify rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -5,15 +5,9 @@ import babel from 'rollup-plugin-babel';
 const pkg = require('./package.json');
 
 const plugins = [
+  babel(),
   resolve(),
-  babel({
-    exclude: 'node_modules/**', // only transpile our source code
-  }),
   commonjs({
-    // non-CommonJS modules will be ignored, but you can also
-    // specifically include/exclude files
-    include: ['node_modules/**', 'gen/parser.js'], // Default: undefined
-
     // if false then skip sourceMap generation for CommonJS modules
     sourceMap: false, // Default: true
   }),
